cb91a527ea7ce79ef299a3db3a07f2061d1511f9
Chris Bryan <chris.bryan2@nhs.net>
2025-03-15 14:34:29 +0000

    Including texts

--- api/api.py
+++ api/api.py
@@ -9,8 +9,10 @@ from . import create_app
 
 app = create_app()
 
-db_path = 'db/YOCal.db'
-assert os.path.exists(db_path), f"{db_path} not found"
+yocal_path = 'db/YOCal.db'
+assert os.path.exists(yocal_path), f"{yocal_path} not found"
+yocal_master_path = 'db/YOCal_Master.db'
+assert os.path.exists(yocal_master_path), f"{yocal_master_path} not found"
 
 
 @app.route("/test")
@@ -29,11 +31,28 @@ def get_date():
     month = request.args.get('month', type=int)
     day = request.args.get('day', type=int)
 
-    return jsonify(_get_data(datetime(year, month, day)))
+    data = _get_data(datetime(year, month, day))
+    data["a_lections"] = _get_lections(data["a_code"])
+    data["g_lections"] = _get_lections(data["g_code"])
+    data["c_lections"] = _get_lections(data["c_code"])
+    data["x_lections"] = _get_lections(data["x_code"])
+    return jsonify(data)
+
+
+def _get_lections(code: str) -> Dict:
+    with sqlite3.connect(yocal_master_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM yocal_lections"
+                       " WHERE code=?",
+                       (code,))
+        result = cursor.fetchall()
+        column_names = [description[0] for description in cursor.description]
+
+    return [dict(zip(column_names, row)) for row in result]
 
 
 def _get_data(date: datetime.date) -> Dict:
-    with sqlite3.connect(db_path) as conn:
+    with sqlite3.connect(yocal_path) as conn:
         cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM Year_{date.year}"
                         " WHERE date = ?",
