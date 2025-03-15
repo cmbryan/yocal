c96b41e0df21f9de55c830cc7aec055f9a7fcb7d
Chris Bryan <chris.bryan2@nhs.net>
2025-03-15 14:03:11 +0000

    expand the date range

--- db/build.sh
+++ db/build.sh
@@ -5,7 +5,7 @@ python -m pip install -r requirements.txt
 rm -f *.db
 
 python ./1_fixed_tables_create_populate.py
-python ./2_year_tables_create.py 2020 2035
+python ./2_year_tables_create.py 2000 2099
 python ./3_insert_ID_date.py
 python ./4_paschalion_cycle.py
 python ./5_menaion_cycle.py
@@ -13,6 +13,6 @@ python ./6_fast_codes.py
 python ./7_tone_eoth.py
 python ./8_master_main_create.py
 python ./9_master_lection_create.py
-python ./10_master_main_tbl_pop.py 2020 2035
+python ./10_master_main_tbl_pop.py 2000 2099
 python ./11_master_lection_pop.py Y
 python ./12_master_lection_correct.py
