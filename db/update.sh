4eb0089c27572f784f1f8fa026e1315b1ea322b8
Chris Bryan <chris.bryan2@nhs.net>
2025-03-18 09:52:38 +0000

    Update changes

--- db/update.sh
+++ db/update.sh
@@ -1,9 +1,15 @@
 #!/bin/env sh
  
+# Pull changes from remote repository
+git pull
+
+# 'Stage' any changes you've made
 git add -u
+
+# Show the changes that are 'staged' and ready to be committed
 git status
 
-echo "\nPlease review 'Changes to be committed' above."
+echo "\n*** Please review 'Changes to be committed' above ***"
 read -p "Do you want to commit and upload these changes? (y/N): " answer
 if [ "$answer" = "y" ]; then
     read -p "Please describe these changes: " commit_msg
